e9a4f72e132322632fbf23d0e1da7e5a545be135
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-16 22:27:55 +0300

    Added orders controller

---
+++ Orderio/Orderio.Web/Controllers/Api/V1/OrdersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Orderio.Web.Controllers.Api.V1
+{
+    [ApiVersion("1.0")]
+    public class OrdersController : ApiBaseController
+    {
+        [HttpGet]
+        public IActionResult GetOrders(int page)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePost()
+        {
+            return Ok();
+        }
+    }
+}
