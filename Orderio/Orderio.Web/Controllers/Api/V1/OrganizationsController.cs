36349b7c8c67c506fdc2d795dd6bf5539a2eb7f8
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-16 22:24:58 +0300

    Added organization controller

---
+++ Orderio/Orderio.Web/Controllers/Api/V1/OrganizationsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Orderio.Web.Controllers.Api.V1
+{
+    [ApiVersion("1.0")]
+    public class OrganizationsController : ApiBaseController
+    {
+        [HttpGet]
+        public IActionResult GetOrganizations(string q)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrganizationById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/departments")]
+        public IActionResult GetOrganizationDepartments(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/departments/{departmentId}")]
+        public IActionResult GetDepartmentById(int id, int departmentId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}/departments/{departmentId}/products")]
+        public IActionResult GetDepartmentProducts(int id, int departmentId)
+        {
+            return Ok();
+        }
+    }
+}
