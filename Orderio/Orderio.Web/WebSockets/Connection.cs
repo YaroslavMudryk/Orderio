ec20bb824acf9cbf25b9016f3ef39451c23101d5
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-17 15:23:15 +0300

    Added part of websocket manager

---
+++ Orderio/Orderio.Web/WebSockets/Connection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace Orderio.Web.WebSockets
+{
+    public class Connection
+    {
+        public WebSocket WebSocket { get; set; }
+        public DateTime ConnectedAt { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
---
+++ Orderio/Orderio.Web/WebSockets/ConnectionManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace Orderio.Web.WebSockets
+{
+    public class ConnectionManager
+    {
+        private ConcurrentDictionary<int, List<Connection>> _connections = new ConcurrentDictionary<int, List<Connection>>();
+
+        public List<Connection> GetUserConnections(int id) => _connections.ContainsKey(id) ? _connections.FirstOrDefault(x => x.Key == id).Value : null;
+
+        public void AddConnection(int userId, Connection connection)
+        {
+            if (_connections.ContainsKey(userId))
+                _connections[userId].Add(connection);
+            else
+                _connections.TryAdd(userId, new List<Connection> { connection });
+        }
+
+        public async Task CloseConnectionAsync(string accessToken)
+        {
+            var userConnections = _connections.FirstOrDefault(x => x.Value.FirstOrDefault(x => x.AccessToken == accessToken) != null);
+            var currentConnection = userConnections.Value.FirstOrDefault(x => x.AccessToken == accessToken);
+            if (_connections[userConnections.Key].Count > 1)
+            {
+                _connections[userConnections.Key].Remove(currentConnection);
+                await currentConnection.WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", System.Threading.CancellationToken.None);
+            }
+            else
+            {
+                _connections.Remove(userConnections.Key, out var connections);
+            }
+        }
+    }
+}
