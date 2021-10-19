472dfde2f9d5e05e2d1a00f80724fdd695359d3d
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-19 16:46:31 +0300

    Added Domain project

---
+++ Orderio/Orderio.Domain/Models/BaseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Orderio.Domain.Models
+{
+    public class BaseModel
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedFromIP { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public string UpdatedFromIP { get; set; }
+    }
+
+    public class BaseModel<TId> : BaseModel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public TId Id { get; set; }
+    }
+}
