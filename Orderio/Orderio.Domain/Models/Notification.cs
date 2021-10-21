e986720cb65501d79b60321febd9cce3ca561c2c
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-21 22:37:48 +0300

    Added notification models

---
+++ Orderio/Orderio.Domain/Models/Notification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Orderio.Domain.Models
+{
+    public class Notification : BaseModel<Guid>
+    {
+        [Required, StringLength(150, MinimumLength = 5)]
+        public string Title { get; set; }
+        [Required, StringLength(3000, MinimumLength = 10)]
+        public string Description { get; set; }
+        [Required]
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public string Payload { get; set; }
+        public bool IsImportant { get; set; }
+        public NotificationType Type { get; set; }
+        public string Icon { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Orderio/Orderio.Domain/Models/NotificationType.cs
@@ -0,0 +1,11 @@
+﻿namespace Orderio.Domain.Models
+{
+    public enum NotificationType
+    {
+        Welcome,
+        Confirm,
+        Login,
+        LoginAttempt,
+        Logout
+    }
+}
