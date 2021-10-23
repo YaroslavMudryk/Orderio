e992c7da6f1cd968e4d0f1bf873335f3701ef2cb
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-24 00:23:34 +0300

    Added IRepository

---
+++ Orderio/Orderio.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using Orderio.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orderio.Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseModel
+    {
+        #region Writable
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> CreateAsync(IEnumerable<TEntity> entities);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> UpdateAsync(IEnumerable<TEntity> entities);
+        Task<TEntity> RemoveAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> RemoveAsync(IEnumerable<TEntity> entities);
+        #endregion
+
+        #region Readable
+        Task<List<TEntity>> GetAllAsync();
+        Task<List<TEntity>> GetListAsync(Predicate<Func<TEntity, bool>> predicate);
+        Task<TEntity> GetAsync(Predicate<Func<TEntity, bool>> predicate);
+        Task<PagedList<TEntity>> GetPagedListAsync(Predicate<Func<TEntity, bool>> predicate, int page = 1);
+        #endregion
+    }
+}
---
+++ Orderio/Orderio.Domain/Interfaces/PagedList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Orderio.Domain.Interfaces
+{
+    public class PagedList<TEntity>
+    {
+        public PagedList(IList<TEntity> items, int totalCount, int page)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            Page = page;
+        }
+        public IList<TEntity> Items { get; }
+        public int TotalCount { get; }
+        public int Page { get; set; }
+    }
+}
