b4f554e519d3076808be06e598f9c7187b6d901f
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-23 19:41:17 +0300

    Added Subscription model

---
+++ Orderio/Orderio.Domain/Models/Subscription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Orderio.Domain.Models
+{
+    public class Subscription : BaseModel<int>
+    {
+        [Required, StringLength(250, MinimumLength = 5)]
+        public string Title { get; set; }
+        [Required]
+        public double Amount { get; set; }
+        [StringLength(2500, MinimumLength = 10)]
+        public string Description { get; set; }
+        [Column(TypeName = "bigint")]
+        public TimeSpan Interval { get; set; }
+        [StringLength(1250, MinimumLength = 10)]
+        public string Image { get; set; }
+    }
+}
