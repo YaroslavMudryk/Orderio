7585bb53c771461e0ddd6793d98be1deb42521d3
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-10-23 20:57:15 +0300

    Added Company model

---
+++ Orderio/Orderio.Domain/Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Orderio.Domain.Models
+{
+    public class Company : BaseModel<int>
+    {
+        [Required, StringLength(300, MinimumLength = 10)]
+        public string Name { get; set; }
+        [StringLength(5000, MinimumLength = 5)]
+        public string Description { get; set; }
+        [StringLength(1500, MinimumLength = 5)]
+        public string Image { get; set; }
+        [StringLength(500, MinimumLength = 5), Url]
+        public string SiteAddress { get; set; }
+        [StringLength(10000, MinimumLength = 10)]
+        public string History { get; set; }
+        [StringLength(15, MinimumLength = 1)]
+        public string CountOfEmployees { get; set; }
+        [StringLength(150, MinimumLength = 5)]
+        public string EmailAddress { get; set; }
+        [StringLength(100, MinimumLength = 3)]
+        public string Phone { get; set; }
+        [StringLength(10000, MinimumLength = 1)]
+        public string AdditionalInfo { get; set; }
+    }
+}
